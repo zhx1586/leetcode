a2773a14e6fc3669c4371eb7919621e20b70ac09
zhx1586 <zhx1586@126.com>
2021-06-06 23:33:34 +0800

    tests ac

--- 0402.RemoveKDigits/remove_k_digits.go
+++ 0402.RemoveKDigits/remove_k_digits.go
@@ -1,5 +1,59 @@
 package removekdigits
 
 func removeKdigits(num string, k int) string {
-	return ""
+	if len(num) == k {
+		return "0"
+	}
+
+	ret := make([]byte, len(num)-k)
+	removeKdigitsCore(num, k, 0, &ret, 0)
+	return leftTrim(string(ret))
+}
+
+func removeKdigitsCore(num string, k int, start int, ret *[]byte, pos int) {
+	if k == 0 {
+		for i := 0; i < len(num)-start; i++ {
+			(*ret)[pos+i] = num[start+i]
+		}
+		return
+	}
+
+	minValue, minValueIndex := findMinValueAndIndex(num, start, start+k+1)
+	(*ret)[pos] = minValue
+	removeKdigitsCore(num, k-(minValueIndex-start), minValueIndex+1, ret, pos+1)
+
+	return
+}
+
+func findMinValueAndIndex(num string, lo int, hi int) (minValue byte, minValueIndex int) {
+	minValue = num[lo]
+	minValueIndex = lo
+
+	for i := lo + 1; i < hi; i++ {
+		if num[i] < minValue {
+			minValue = num[i]
+			minValueIndex = i
+		}
+	}
+
+	return minValue, minValueIndex
+}
+
+func leftTrim(num string) string {
+	count := 0
+	for i := 0; i < len(num)-1; i++ {
+		if num[i] == '0' {
+			count++
+		} else {
+			break
+		}
+	}
+	if count == 0 {
+		return num
+	}
+	ret := make([]byte, len(num)-count)
+	for i := count; i < len(num); i++ {
+		ret[i-count] = num[i]
+	}
+	return string(ret)
 }
