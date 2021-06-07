33673f51710001d7fba22a8cbd86746f29899ca8
zhx1586 <zhx1586@126.com>
2021-06-07 22:15:51 +0800

    failed 2 ac

--- 0403.FrogJump/frog_jump.go
+++ 0403.FrogJump/frog_jump.go
@@ -1,32 +1,43 @@
 package frogjump
 
+import "fmt"
+
+var cache map[string]bool
+
 func canCross(stones []int) bool {
 	if stones[1]-stones[0] != 1 {
 		return false
 	}
-	return canCrossCore(stones, 1, 1)
+	return canCrossCore(stones, 1, 1, map[string]bool{})
 }
 
-func canCrossCore(stones []int, pos int, k int) bool {
+func canCrossCore(stones []int, pos int, k int, cache map[string]bool) bool {
 	if pos == len(stones)-1 {
 		return true
+	}
+	key := fmt.Sprintf("%d-%d", pos, k)
+	if cached, ok := cache[key]; ok {
+		return cached
 	}
 	for i := pos + 1; i < len(stones); i++ {
 		if k-1 > 0 && stones[i] == stones[pos]+k-1 {
 			//fmt.Printf("jump k-1: %+v, %d, %d, (%d->%d)\n", stones, pos, k, stones[pos], stones[i])
-			if canCrossCore(stones, i, k-1) {
+			if canCrossCore(stones, i, k-1, cache) {
+				cache[key] = true
 				return true
 			}
 		}
 		if stones[i] == stones[pos]+k {
 			//fmt.Printf("jump k: %+v, %d, %d, (%d->%d)\n", stones, pos, k, stones[pos], stones[i])
-			if canCrossCore(stones, i, k) {
+			if canCrossCore(stones, i, k, cache) {
+				cache[key] = true
 				return true
 			}
 		}
 		if stones[i] == stones[pos]+k+1 {
 			//fmt.Printf("jump k+1: %+v, %d, %d, (%d->%d)\n", stones, pos, k, stones[pos], stones[i])
-			if canCrossCore(stones, i, k+1) {
+			if canCrossCore(stones, i, k+1, cache) {
+				cache[key] = true
 				return true
 			}
 		}
@@ -34,5 +45,6 @@ func canCrossCore(stones []int, pos int, k int) bool {
 			break
 		}
 	}
+	cache[key] = false
 	return false
 }
