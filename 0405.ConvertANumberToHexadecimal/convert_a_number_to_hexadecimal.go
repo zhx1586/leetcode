32153fcc40bef383b95829cc940644a6cb070b88
zhx1586 <zhx1586@126.com>
2021-06-09 23:41:23 +0800

    tests ac

---
+++ 0405.ConvertANumberToHexadecimal/convert_a_number_to_hexadecimal.go
@@ -0,0 +1,36 @@
+package convertanumbertohexadecimal
+
+import (
+	"strings"
+)
+
+func toHex(num int) string {
+	hex := map[int]byte{
+		0x0: '0',
+		0x1: '1',
+		0x2: '2',
+		0x3: '3',
+		0x4: '4',
+		0x5: '5',
+		0x6: '6',
+		0x7: '7',
+		0x8: '8',
+		0x9: '9',
+		0xa: 'a',
+		0xb: 'b',
+		0xc: 'c',
+		0xd: 'd',
+		0xe: 'e',
+		0xf: 'f',
+	}
+	length := 8
+	ret := make([]byte, length)
+	for i := 0; i < length; i++ {
+		shift := (length - 1 - i) * 4
+		mask := 0xf << shift
+		intersection := num & mask
+		ret[i] = hex[intersection>>shift]
+	}
+
+	return strings.TrimLeft(string(ret), "0")
+}
---
+++ 0405.ConvertANumberToHexadecimal/convert_a_number_to_hexadecimal_test.go
@@ -0,0 +1,46 @@
+package convertanumbertohexadecimal
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestToHex(t *testing.T) {
+	type args struct {
+		num int
+	}
+	type want struct {
+		ret string
+	}
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			name: "Example 1",
+			args: args{
+				num: 26,
+			},
+			want: want{
+				ret: "1a",
+			},
+		},
+		{
+			name: "Example 2",
+			args: args{
+				num: -1,
+			},
+			want: want{
+				ret: "ffffffff",
+			},
+		},
+	}
+	for _, tt := range tests {
+		got := toHex(tt.args.num)
+		if !reflect.DeepEqual(got, tt.want.ret) {
+			t.Errorf("TestToHex %s failed: (got: %+v, want: %+v)",
+				tt.name, got, tt.want.ret)
+		}
+	}
+}
