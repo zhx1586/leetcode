1eea72f8d4b1a726f8eb00d25f5f5ea18cb3c9cb
zhx1586 <zhx1586@126.com>
2021-06-15 23:13:23 +0800

    tests ac

--- 0416.PartitionEqualSubsetSum/partition_equal_subset_sum.go
+++ 0416.PartitionEqualSubsetSum/partition_equal_subset_sum.go
@@ -1,6 +1,31 @@
 package partitionequalsubsetsum
 
-func canPartition(nums []int) bool {
+import "sort"
 
-	return false
+func canPartition(nums []int) bool {
+	sum := 0
+	for _, n := range nums {
+		sum = sum + n
+	}
+	if sum%2 == 1 {
+		return false
+	}
+
+	sort.Sort(sort.IntSlice(nums))
+
+	return canPartitionCore(nums, 0, sum/2)
+}
+
+func canPartitionCore(nums sort.IntSlice, pos int, target int) bool {
+	if pos == len(nums) {
+		return false
+	}
+	if nums[pos] > target {
+		return false
+	} else if nums[pos] < target {
+		return canPartitionCore(nums, pos+1, target) ||
+			canPartitionCore(nums, pos+1, target-nums[pos])
+	} else {
+		return true
+	}
 }
