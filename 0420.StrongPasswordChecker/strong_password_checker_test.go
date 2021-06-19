4a178b5fcc81217472016d5b64b252f537ee5761
zhx1586 <zhx1586@126.com>
2021-06-19 20:02:48 +0800

    merge 0420

---
+++ 0420.StrongPasswordChecker/strong_password_checker_test.go
@@ -0,0 +1,73 @@
+package strongpasswordchecker
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestStrongPasswordChecker(t *testing.T) {
+	type args struct {
+		password string
+	}
+	type want struct {
+		ret int
+	}
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			name: "Example 1",
+			args: args{
+				password: "a",
+			},
+			want: want{
+				ret: 5,
+			},
+		},
+		{
+			name: "Example 2",
+			args: args{
+				password: "aA1",
+			},
+			want: want{
+				ret: 3,
+			},
+		},
+		{
+			name: "Example 3",
+			args: args{
+				password: "1337C0d3",
+			},
+			want: want{
+				ret: 0,
+			},
+		},
+		{
+			name: "Failed 1",
+			args: args{
+				password: "aaa111",
+			},
+			want: want{
+				ret: 2,
+			},
+		},
+		{
+			name: "Failed 2",
+			args: args{
+				password: "aaaB1",
+			},
+			want: want{
+				ret: 1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		got := strongPasswordChecker(tt.args.password)
+		if !reflect.DeepEqual(got, tt.want.ret) {
+			t.Errorf("TestStrongPasswordChecker %s failed: (got: %+v, want: %+v)",
+				tt.name, got, tt.want.ret)
+		}
+	}
+}
