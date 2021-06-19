daaa8d51aedd064ccc44a9c036b1b31804432e3d
zhx1586 <zhx1586@126.com>
2021-06-19 20:37:27 +0800

    tests ac

---
+++ 0421.MaximumXOROfTwoNumbersInAnArray/maximum_xor_of_two_numbers_in_an_array.go
@@ -0,0 +1,19 @@
+package maximumxoroftwonumbersinanarray
+
+import "math"
+
+func findMaximumXOR(nums []int) int {
+	dp := nums[0]
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if nums[i]^nums[j] > dp {
+				dp = nums[i] ^ nums[j]
+				if dp == int(math.MaxInt32) {
+					return dp
+				}
+			}
+		}
+	}
+
+	return dp
+}
---
+++ 0421.MaximumXOROfTwoNumbersInAnArray/maximum_xor_of_two_numbers_in_an_array_test.go
@@ -0,0 +1,75 @@
+package maximumxoroftwonumbersinanarray
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/zhx1586/leetcode/utils"
+)
+
+func TestFindMaximumXOR(t *testing.T) {
+	type args struct {
+		nums string
+	}
+	type want struct {
+		ret int
+	}
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			name: "Example 1",
+			args: args{
+				nums: "[3,10,5,25,2,8]",
+			},
+			want: want{
+				ret: 28,
+			},
+		},
+		{
+			name: "Example 2",
+			args: args{
+				nums: "[0]",
+			},
+			want: want{
+				ret: 0,
+			},
+		},
+		{
+			name: "Example 3",
+			args: args{
+				nums: "[2,4]",
+			},
+			want: want{
+				ret: 6,
+			},
+		},
+		{
+			name: "Example 4",
+			args: args{
+				nums: "[8,10,2]",
+			},
+			want: want{
+				ret: 10,
+			},
+		},
+		{
+			name: "Example 5",
+			args: args{
+				nums: "[14,70,53,83,49,91,36,80,92,51,66,70]",
+			},
+			want: want{
+				ret: 127,
+			},
+		},
+	}
+	for _, tt := range tests {
+		got := findMaximumXOR(IntSlice(tt.args.nums))
+		if !reflect.DeepEqual(got, tt.want.ret) {
+			t.Errorf("TestFindMaximumXOR %s failed: (got: %+v, want: %+v)",
+				tt.name, got, tt.want.ret)
+		}
+	}
+}
