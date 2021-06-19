678051e63bd10022c57c754f1d21f5063a691332
zhx1586 <zhx1586@126.com>
2021-06-19 22:28:37 +0800

    all ac

--- 0421.MaximumXOROfTwoNumbersInAnArray/maximum_xor_of_two_numbers_in_an_array.go
+++ 0421.MaximumXOROfTwoNumbersInAnArray/maximum_xor_of_two_numbers_in_an_array.go
@@ -15,15 +15,10 @@ func findMaximumXOR(nums []int) int {
 	}
 	all := or & (^and)
 
-	//fmt.Println(len(nums))
-	//fmt.Printf("or:%b, and:%b, all:%b\n", or, and, all)
-
 	for t := all; t >= 0; t-- {
 		if t|all == all {
-			//fmt.Printf("t:%b\n", t)
 			for _, n := range nums {
 				if _, ok := set[n^t]; ok {
-					//fmt.Printf("(%b,%b)\n", n, n^t)
 					return t
 				}
 			}
