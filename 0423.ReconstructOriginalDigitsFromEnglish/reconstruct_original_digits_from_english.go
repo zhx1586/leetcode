4e690bd2114a010c82500c865ace3404d3950eb8
zhx1586 <zhx1586@126.com>
2021-06-20 12:16:39 +0800

    time

--- 0423.ReconstructOriginalDigitsFromEnglish/reconstruct_original_digits_from_english.go
+++ 0423.ReconstructOriginalDigitsFromEnglish/reconstruct_original_digits_from_english.go
@@ -4,27 +4,29 @@ func originalDigits(s string) string {
 	code := []string{
 		"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 	}
-	counts := map[byte]int{
-		'e': 0, // zero, one, three, five, seven, eight, nine
-		'g': 0, // eight
-		'f': 0, // four, five
-		'i': 0, // five, six, eight, nine
-		'h': 0, // three, eight
-		'o': 0, // zero, one, two, four
-		'n': 0, // one, seven, nine
-		's': 0, // six, seven
-		'r': 0, // zero, three, four
-		'u': 0, // four
-		't': 0, // two, three, eight
-		'w': 0, // two
-		'v': 0, // five, seven
-		'x': 0, // six
-		'z': 0, // zero
-	}
+	/*
+		counts := map[byte]int{
+			'e': 0, // zero, one, three, five, seven, eight, nine
+			'g': 0, // eight
+			'f': 0, // four, five
+			'i': 0, // five, six, eight, nine
+			'h': 0, // three, eight
+			'o': 0, // zero, one, two, four
+			'n': 0, // one, seven, nine
+			's': 0, // six, seven
+			'r': 0, // zero, three, four
+			'u': 0, // four
+			't': 0, // two, three, eight
+			'w': 0, // two
+			'v': 0, // five, seven
+			'x': 0, // six
+			'z': 0, // zero
+		}*/
+	counts := make([]int, 26)
 	digits, length := make([]int, 10), 0
 
 	for i := 0; i < len(s); i++ {
-		counts[s[i]]++
+		counts[s[i]-'a']++
 	}
 
 	seq := []struct {
@@ -43,10 +45,10 @@ func originalDigits(s string) string {
 		{'i', 9},
 	}
 	for _, s := range seq {
-		digits[s.num] = counts[s.letter]
+		digits[s.num] = counts[s.letter-'a']
 		length = length + digits[s.num]
 		for i := 0; i < len(code[s.num]); i++ {
-			counts[code[s.num][i]] = counts[code[s.num][i]] - digits[s.num]
+			counts[code[s.num][i]-'a'] = counts[code[s.num][i]-'a'] - digits[s.num]
 		}
 	}
 
