42929b91b0d013b23d23688156f8c9ae02839be6
zhx1586 <zhx1586@126.com>
2021-06-26 18:59:12 +0800

    op time

--- 0424.LongestRepeatingCharacterReplacement/longest_repeating_character_replacement.go
+++ 0424.LongestRepeatingCharacterReplacement/longest_repeating_character_replacement.go
@@ -3,25 +3,24 @@ package longestrepeatingcharacterreplacement
 func characterReplacement(s string, k int) int {
 	ret, n := 0, len(s)
 	lo, hi := 0, 0
-	counts := map[byte]int{s[0]: 1}
-	//fmt.Println(s)
+	counts := make([]int, 26)
+	counts[s[0]-'A'] = 1
 
 	for hi < n {
-		//fmt.Printf("lo:%d, hi:%d, ret:%d, counts:%v\n", lo, hi, ret, counts)
 		validate := isValiade(counts, hi-lo+1, k)
 		if validate {
 			ret = max(ret, hi-lo+1)
 			hi++
 			if hi < n {
-				counts[s[hi]]++
+				counts[s[hi]-'A']++
 			}
 		} else if lo < hi {
-			counts[s[lo]]--
+			counts[s[lo]-'A']--
 			lo++
 		} else {
 			hi++
 			if hi < n {
-				counts[s[hi]]++
+				counts[s[hi]-'A']++
 			}
 		}
 		if hi == n {
@@ -32,7 +31,7 @@ func characterReplacement(s string, k int) int {
 	return ret
 }
 
-func isValiade(counts map[byte]int, interval int, k int) bool {
+func isValiade(counts []int, interval int, k int) bool {
 	for _, count := range counts {
 		if count+k >= interval {
 			return true
